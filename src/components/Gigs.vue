1eb485ad9089c621fa60b5aa680631b3924f2e82
Alberto Gualis <agualis@gmail.com>
2017-09-22 12:43:42 +0200

    First sample with list of gigs

---
+++ src/components/Gigs.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="layout-padding row justify-center">
+    <q-toolbar color="primary">
+      MOSICA
+    </q-toolbar>
+
+    <div style="width: 100%">
+      <q-list>
+        <div v-for="day in gigsByDay">
+        <q-list-header>{{day.day}}</q-list-header>
+          <q-item v-for="gig in day.gigs" @click="goTo(gig)"
+                  :key="gig.id" highlight>
+            {{gig.title}} - {{gig.place}}
+          </q-item>
+        </div>
+      </q-list>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import {
+    QList,
+    QListHeader,
+    QItem,
+    QToolbar
+  } from 'quasar'
+
+  import fetchJsonp from 'fetch-jsonp'
+  import * as mosicaCore from 'mosica-core'
+  import { HttpClient } from '../services/HttpClient'
+
+  export default {
+    components: {
+      QList,
+      QListHeader,
+      QItem,
+      QToolbar
+    },
+    data () {
+      return {
+        gigService: new mosicaCore.GigService(HttpClient(fetchJsonp), new mosicaCore.Matcher()),
+        gigsByDay: []
+      }
+    },
+    async created() {
+      this.gigsByDay = await this.gigService.retrieveNextGigs()
+    },
+    methods: {
+      goTo(gig) {
+        console.log('Moviendo a página de concierto', gig)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/services/HttpClient.js
@@ -0,0 +1,9 @@
+export const HttpClient = (fetchClient) => {
+  return { get }
+
+  async function get (url) {
+    const response = await fetchClient(url)
+    const json = await response.json()
+    return {body: json}
+  }
+}
