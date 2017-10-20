3dc4612b4bca507394466647ace711f555879f3b
Alberto Gualis <agualis@gmail.com>
2017-10-20 11:24:46 +0200

    Remove synchronous-promise

--- src/app/__mocks__/mosica-core.js
+++ src/app/__mocks__/mosica-core.js
@@ -1,12 +1,9 @@
 import { fakeGigsByDay } from './gigs-sample'
 const mosicaCore = jest.genMockFromModule('mosica-core')
-import {SynchronousPromise} from 'synchronous-promise'
 
 class GigService {
   retrieveNextGigs() {
-    // return Promise.resolve(fakeGigsByDay)
-    //CREO QUE ESTO NO FUNCIONA PERO HABRIA QUE DARLE ALGUN PASE MAS
-    return SynchronousPromise.resolve(fakeGigsByDay)
+    return Promise.resolve(fakeGigsByDay)
   }
 }
 
