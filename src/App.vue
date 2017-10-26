fc92506a1308c1f976c4059e10e2778144003507
Alberto Gualis <agualis@gmail.com>
2017-10-26 15:33:33 +0200

    Change route transition styles

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="q-app">
     <div class="layout-padding row justify-center">
       <Toolbar slot="header"></Toolbar>
-      <transition name="fade" mode="out-in">
-        <router-view />
+      <transition name="fade">
+        <router-view class="layout-view"/>
       </transition>
     </div>
   </div>
@@ -17,5 +17,11 @@ export default {}
 </script>
 
 <style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s
+  }
 
+  .fade-enter, .fade-leave-to {
+    opacity: 0
+  }
 </style>
