419587875358d9e5d514c2ceb7a134f4d6aaa4a7
Alberto Gualis <agualis@gmail.com>
2017-10-28 18:57:37 +0200

    Show message when no gigs available

--- src/app/pages/GigsToday/GigsToday.vue
+++ src/app/pages/GigsToday/GigsToday.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="full-width">
-      <div v-if="gigsToday">
-        <Day :day="gigsToday"/>
-      </div>
+        <Day :day="gigsToday" v-if="gigsToday"/>
+        <h3 v-else>No gigs for today, why don't you go to the cinema?</h3>
     </div>
 </template>
 
