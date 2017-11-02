6b4334b7fa4dd8a6775a07726472192c58e06026
Alberto Gualis <agualis@gmail.com>
2017-11-02 11:28:36 +0100

    Add sample of component in functional format

---
+++ src/app/pages/Days/GigRowFunctional.vue
@@ -0,0 +1,5 @@
+<template functional>
+  <q-item @click="props.onClick(props.gig)" :key="props.gig.id" highlight link striped>
+    {{props.gig.title}} - {{props.gig.place}}
+  </q-item>
+</template>
