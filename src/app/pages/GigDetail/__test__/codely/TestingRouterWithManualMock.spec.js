19b447caeee56fed628c307765cdd6aeef426a37
Alberto Gualis <agualis@gmail.com>
2017-11-03 17:48:39 +0100

    Add test to demonstrate bypassing router getParams

--- src/app/pages/GigDetail/__test__/codely/TestingRouterWithManualMock.spec.js
+++ src/app/pages/GigDetail/__test__/codely/TestingRouterWithManualMock.spec.js
@@ -1,14 +1,15 @@
 import GigDetail from '@/app/pages/GigDetail/GigDetail.vue'
 import { mount } from 'vue-test-utils'
-import { FIRST_GIG } from '../../../services/__mocks__/gigs-sample'
+import VueRouter from 'vue-router'
+import { FIRST_GIG } from '../../../../services/__mocks__/gigs-sample'
 
 jest.mock('@/app/services/jota-api')
-jest.mock('@/app/services/JotaRouter')
 
 describe('Gig Detail', () => {
 
-  it('renders details from a Gig', async () => {
-    const wrapper = mount(GigDetail)
+  it('renders details from a Gig with router after', async () => {
+    const routerSpy = new VueRouter()
+    const wrapper = mount(GigDetail, { router: routerSpy })
     await wait()
     expect(wrapper.text()).toContain(FIRST_GIG.title)
     expect(wrapper.text()).toContain(FIRST_GIG.place)
