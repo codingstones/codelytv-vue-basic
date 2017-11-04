d79b20090a09b28b4f16c6031e3c46193b4a2266
Alberto Gualis <agualis@gmail.com>
2017-11-04 11:24:51 +0100

    Add codely test to show differences between then and async/await

---
+++ src/app/pages/GigDetail/__test__/codely/TestingWithBackendStubbedByJest.spec.js
@@ -0,0 +1,29 @@
+import GigDetail from '@/app/pages/GigDetail/GigDetail.vue'
+import { mount } from 'vue-test-utils'
+import { FIRST_GIG } from '../../../../services/__mocks__/gigs-sample'
+
+jest.mock('@/app/services/JotaRouter')
+jest.mock('@/app/services/jota-api')
+
+describe('Gig Detail', () => {
+
+  it('renders details from a Gig (WITH async/await)', async () => {
+    const wrapper = mount(GigDetail)
+    await wait()
+
+    expect(wrapper.text()).toContain(FIRST_GIG.title)
+    expect(wrapper.text()).toContain(FIRST_GIG.place)
+  })
+
+  it('renders details from a Gig (WITHOUT async/await)', () => {
+    const wrapper = mount(GigDetail)
+    return wait().then(() => {
+      expect(wrapper.text()).toContain(FIRST_GIG.title)
+      expect(wrapper.text()).toContain(FIRST_GIG.place)
+    })
+  })
+})
+
+function wait() {
+  return new Promise(resolve => setImmediate(resolve))
+}
