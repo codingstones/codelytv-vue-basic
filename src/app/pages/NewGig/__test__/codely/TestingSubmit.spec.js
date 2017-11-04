860d6f684023870cfa2d0b7ab9670ff866d8ea39
Alberto Gualis <agualis@gmail.com>
2017-11-04 20:33:00 +0100

    Add codely tests for Submit Form and PageObjects example

---
+++ src/app/pages/NewGig/__test__/codely/TestingSubmit.spec.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import { mount } from 'vue-test-utils'
+import NewGig from '@/app/pages/NewGig/NewGig.vue'
+import TextInput from '@/app/shared-components/TextInput.vue'
+import DateTimeInput from '@/app/shared-components/DateTimeInput.vue'
+import FormButton from '@/app/shared-components/FormButton.vue'
+import { createGig as createGigSpy } from '@/app/services/jota-api'
+import { createGigPayload } from '@/app/services/jota-payloads'
+jest.mock('@/app/services/jota-api')
+jest.mock('@/app/services/JotaRouter')
+
+describe('New Gig Form', () => {
+  const FUTURE_DATETIME = '3000/10/27'
+
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(NewGig)
+  })
+
+  describe('When clicking save button', async () => {
+    beforeEach(async () => {
+      // Write valid title
+      let input = wrapper.find(TextInput)
+      input.vm.change(nameWithValidLength())
+
+      // Select valid datetime
+      const datetimeInput = wrapper.find(DateTimeInput)
+      datetimeInput.vm.change(FUTURE_DATETIME)
+
+      await wait()
+
+      // Click save button
+      wrapper.find(FormButton).trigger('click')
+
+      await wait()
+    })
+
+    it('calls backend with appropriate command', async () => {
+      // This will be also tested in happy path but in this integration tests we can check all strange cases
+      // faster and cheaper
+      expect(createGigSpy).toHaveBeenCalledWith(createGigPayload(nameWithValidLength(), FUTURE_DATETIME))
+    })
+  })
+})
+
+function nameWithValidLength() {
+  return nameWithLength(5)
+}
+
+function nameWithLength(length) {
+  return 'x'.repeat(length)
+}
+
+function wait() {
+  return Vue.nextTick()
+}
