daba4e9115956eec61822ac7dc62aa10cab3c789
Alberto Gualis <agualis@gmail.com>
2017-11-08 13:57:18 +0100

    Add single spec with page object for codely

---
+++ src/app/pages/NewGig/__test__/codely/TestingValidationsWithPageObjects.spec.js
@@ -0,0 +1,31 @@
+import { mount } from 'vue-test-utils'
+import NewGig from '@/app/pages/NewGig/NewGig.vue'
+import NewGigPage from '../../../../__page_objects__/NewGigPageObject'
+
+jest.mock('@/app/services/jota-api')
+
+describe('New Gig Form', () => {
+
+  let wrapper, page
+  beforeEach(() => {
+    wrapper = mount(NewGig)
+    page = new NewGigPage(wrapper)
+  })
+
+  describe('shows validation error', () => {
+
+    it('when title is too short', () => {
+      page.writeName(tooShortName())
+
+      expect(page.text()).toContain('Minimum 5 characters.')
+    })
+  })
+})
+
+function tooShortName() {
+  return nameWithLength(3)
+}
+
+function nameWithLength(length) {
+  return 'x'.repeat(length)
+}
